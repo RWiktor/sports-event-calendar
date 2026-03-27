5e440e265f822b02efdd02f03bc40ef3f4bd7f8f
RWiktor <wiktor.rybakowski@gmail.com>
2026-03-27 13:52:49 +0100

    feat(sports): add sports list endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SportsModule } from './sports/sports.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, EventsModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, EventsModule, SportsModule],
   controllers: [],
   providers: [],
 })
