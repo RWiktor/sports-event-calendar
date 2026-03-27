0f8913d2791532bf180ece98e3f4f9a07aa7e395
RWiktor <wiktor.rybakowski@gmail.com>
2026-03-27 01:07:27 +0100

    feat: integrate Swagger for API documentation and add necessary dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Sports Event Calendar')
+    .setDescription('REST API for the sports event calendar')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap();
